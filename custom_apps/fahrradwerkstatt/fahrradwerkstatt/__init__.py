a0cb2f1387d8ee6d88874eb87dfeb1c2b78cd00a
nc-agency <info.ncagency@gmail.com>
2025-05-04 23:45:50 +0200

    Erstelle Grundstruktur für benutzerdefinierte Anwendung

---
+++ custom_apps/fahrradwerkstatt/fahrradwerkstatt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Fahrradwerkstatt App für ERPNext
+# 
+# Erstellt: 04.05.2025
+# 
+# Dieses Modul enthält die Initialisierungsdatei für die Fahrradwerkstatt-App.
+
+from __future__ import unicode_literals
+
+__version__ = '0.0.1'
